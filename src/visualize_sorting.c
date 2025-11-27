cd14ffa2ac7ea845c4ed4f6ead7f1338d94944bd
arghpy <subaandrei2@gmail.com>
2025-11-27 11:49:44 +0200

    feat: showcase better other application options without cluttering the window

--- src/visualize_sorting.c
+++ src/visualize_sorting.c
@@ -17,15 +17,16 @@
 #define FONT_SIZE 25
 #define FONT_POSITION ((Vector2) {20, 20})
 
-#define WINDOW_SCALE 100
-#define SCREEN_WIDTH (WINDOW_SCALE*16)
+#define WINDOW_SCALE  100
+#define SCREEN_WIDTH  (WINDOW_SCALE*16)
 #define SCREEN_HEIGHT (WINDOW_SCALE*9)
-#define WINDOW_COLOR ((Color) {18, 18, 18, 0xFF})
+#define WINDOW_COLOR  ((Color) {18, 18, 18, 0xFF})
 
 // Interactive colors
 Color default_color = WHITE;
 Color fps_color = BLUE;
 Color paused_color = WHITE;
+Color show_menu_color = WHITE;
 
 char fps_text[50] = {0};
 
@@ -34,53 +35,76 @@ typedef struct {
   Color *color;
 } Text;
 
-Text *algorithm_options[] = {
-  &((Text){ .text = bubble_sort_state.text,    .color = &bubble_sort_state.color}),
-  &((Text){ .text = cocktail_sort_state.text,  .color = &cocktail_sort_state.color}),
-  &((Text){ .text = insertion_sort_state.text, .color = &insertion_sort_state.color}),
+Text quick_menu[] = {
+  (Text){ .text = "[F5] show more options",  .color = &show_menu_color },
+  (Text){ .text = bubble_sort_state.text,    .color = &bubble_sort_state.color    },
+  (Text){ .text = cocktail_sort_state.text,  .color = &cocktail_sort_state.color  },
+  (Text){ .text = insertion_sort_state.text, .color = &insertion_sort_state.color },
 };
 
-Text *button_options[] = {
-  &((Text){ .text = "[F5] show this menu",         .color = &default_color }),
-  &((Text){ .text = "[space] pause",               .color = &paused_color }),
-  &((Text){ .text = "[r] reset array",             .color = &default_color }),
-  &((Text){ .text = "[left arrow] slow down",      .color = &default_color }),
-  &((Text){ .text = "[right arrow] speed up",      .color = &default_color }),
-  &((Text){ .text = "[up arrow] increase array",   .color = &default_color }),
-  &((Text){ .text = "[down arrow] decrease array", .color = &default_color }),
+Text other_options[] = {
+  (Text){ .text = "[space] pause",               .color = &paused_color  },
+  (Text){ .text = "[r] reset array",             .color = &default_color },
+  (Text){ .text = "[left arrow] slow down",      .color = &default_color },
+  (Text){ .text = "[right arrow] speed up",      .color = &default_color },
+  (Text){ .text = "[up arrow] increase array",   .color = &default_color },
+  (Text){ .text = "[down arrow] decrease array", .color = &default_color },
 };
 
-typedef ut_da_declare(Text) Menu;
-Menu menu = {0};
-
-void populate_options(Menu *m)
+void draw_other_options(Font *font)
 {
-  for (size_t i = 0; i < ARRAY_LEN(algorithm_options); i++)
-    ut_da_push(m, *algorithm_options[i]);
+  // Blur
+  DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(WHITE, 0.3f));
 
-  for (size_t i = 0; i < ARRAY_LEN(button_options); i++)
-    ut_da_push(m, *button_options[i]);
+  // Draw rectangle
+  size_t text_max_length_index = 0;
+  float text_max_length = MeasureTextEx(*font, other_options[text_max_length_index].text, FONT_SIZE, 0).x;
+
+  for (size_t i = 1; i < ARRAY_LEN(other_options); i++) {
+    float text_length = MeasureTextEx(*font, other_options[i].text, FONT_SIZE, 0).x;
+    if (text_length > text_max_length) {
+      text_max_length = text_length;
+      text_max_length_index = i;
+    }
+  }
+  float padding = font->baseSize * 0.5f;
+  Rectangle menu = {
+    .x = GetScreenWidth()/2.0f - (text_max_length + padding*2)/2.0f,
+    .y = GetScreenHeight()/2.0f - (ARRAY_LEN(other_options) * font->baseSize + padding*2)/2.0f,
+    .width = text_max_length + padding*2,
+    .height = ARRAY_LEN(other_options) * font->baseSize + padding*2,
+  };
+  DrawRectangleRec(menu, WINDOW_COLOR);
+  DrawRectangleLinesEx(menu, 1.0f, GREEN);
+
+  // Draw options
+  for (size_t i = 0; i < ARRAY_LEN(other_options); i++) {
+    Vector2 position = {
+      .x = menu.x + padding,
+      .y = menu.y + font->baseSize * i + padding,
+    };
+    DrawTextEx(*font, other_options[i].text, position, FONT_SIZE, 0, *other_options[i].color);
+  }
 }
 
-void draw_options(Menu *m, Font *font, bool show_menu)
+void draw_quick_menu(Font *font)
 {
-  size_t counter = show_menu ? m->count : ARRAY_LEN(algorithm_options);
-  for (size_t i = 0; i < counter; i++) {
+  for (size_t i = 0; i < ARRAY_LEN(quick_menu); i++) {
     Vector2 position = {
       .x = FONT_POSITION.x,
       .y = FONT_POSITION.y + font->baseSize * i,
     };
-    DrawTextEx(*font, m->items[i].text, position, FONT_SIZE, 0, *m->items[i].color);
+    DrawTextEx(*font, quick_menu[i].text, position, FONT_SIZE, 0, *quick_menu[i].color);
   }
 }
 
-void reset_bars(Bars *bs, Menu *m, Font *font)
+void reset_bars(Bars *bs, Font *font)
 {
   int screen_width = GetScreenWidth();
   int screen_height = GetScreenHeight();
   ut_da_reset(bs);
   for (size_t i = 0; i < screen_width/BAR_LENGTH; i++)
-    ut_da_push(bs, ((Bar) {.height = rand()%((int)(screen_height - FONT_POSITION.y - font->baseSize * m->count)), .color = WHITE}));
+    ut_da_push(bs, ((Bar) {.height = rand()%((int)(screen_height - FONT_POSITION.y - font->baseSize * (int)ARRAY_LEN(quick_menu))), .color = WHITE}));
 }
 
 int main(void)
@@ -93,11 +117,9 @@ int main(void)
   char *font_path = "./thirdparty/assets/fonts/JetBrainsMono-Regular.ttf";
   Font font = LoadFontEx(font_path, FONT_SIZE, NULL, 0);
 
-  populate_options(&menu);
-
   Bars bs = {0};
   srand(time(0));
-  reset_bars(&bs, &menu, &font);
+  reset_bars(&bs, &font);
 
   size_t k = 0;
   bool paused = false;
@@ -110,10 +132,11 @@ int main(void)
     if (IsKeyPressed(KEY_F5)) show_menu = !show_menu;
 
     if (IsKeyPressed(KEY_SPACE)) paused = !paused;
-    paused_color = paused ? RED: WHITE;
+    show_menu_color = show_menu ? GREEN : WHITE;
+    paused_color = paused ? RED : WHITE;
 
     if (IsKeyPressed(KEY_R)) {
-      reset_bars(&bs, &menu, &font);
+      reset_bars(&bs, &font);
       k = 0;
       sorted = false;
 
@@ -127,7 +150,7 @@ int main(void)
     if (IsKeyPressed(KEY_UP)) {
       if (BAR_LENGTH > 3)
         BAR_LENGTH -= 2;
-      reset_bars(&bs, &menu, &font);
+      reset_bars(&bs, &font);
       k = 0;
       reset_bubble_sort();
       reset_cocktail_sort();
@@ -137,7 +160,7 @@ int main(void)
 
     if (IsKeyPressed(KEY_DOWN)) {
       BAR_LENGTH += 2;
-      reset_bars(&bs, &menu, &font);
+      reset_bars(&bs, &font);
       k = 0;
 
       reset_bubble_sort();
@@ -203,8 +226,9 @@ int main(void)
     BeginDrawing();
     {
       ClearBackground(WINDOW_COLOR);
-      draw_options(&menu, &font, show_menu);
+      draw_quick_menu(&font);
       draw_bars(&bs);
+      if (show_menu) draw_other_options(&font);
     }
     EndDrawing();
   }
